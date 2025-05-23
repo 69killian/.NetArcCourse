af2a18398c6b2d92675109e941550d62fff5a256
Killian <martykillian7@gmail.com>
2025-05-23 15:35:04 +0200

    first commit

---
+++ output/program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write() = écrit dans la même ligne
+            Console.Write('Hello');
+            //WriteLine() = écrit dans une nouvelle ligne
+            Console.WriteLine('Hello');
+        }
+    }
+}
