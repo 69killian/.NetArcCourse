216994f8bee5312e20b3e9c7aa5f7c8ba5bb51ff
Killian <martykillian7@gmail.com>
2025-05-23 15:42:01 +0200

    outputs in c#

--- output/program.cs
+++ output/program.cs
@@ -6,10 +6,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Write() = écrit dans la même ligne
-            Console.Write('Hello');
-            //WriteLine() = écrit dans une nouvelle ligne
-            Console.WriteLine('Hello');
+            // Write() = écrit dans la même ligne
+            Console.Write("Hello");
+            // WriteLine() = écrit dans une nouvelle ligne
+            Console.WriteLine("Hello");
+            /*
+            * this is 
+            * multiline comment
+            */
+
+            // \n = new line
+            // \t = tab
+            // \b = backspace
+            Console.WriteLine("Killian\nKillian\nKillian");
+            Console.WriteLine("\tKillian");
+            Console.WriteLine("Killian\b Marty");
+
+            // pause le programme
+            Console.ReadKey();
         }
     }
 }
+
+// .\run_csharp. 
+// pour lancer le code
