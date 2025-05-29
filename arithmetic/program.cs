6630f34b9691188005aeef93a537fcd4d9aa25aa
Killian <martykillian7@gmail.com>
2025-05-29 10:03:30 +0200

    arithmetics

---
+++ arithmetic/program.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int friends = 5;
+
+            friends++;
+            friends += 1;
+            friends = friends + 1;
+
+            friends--;
+            friends -= 1;
+            friends = friends - 1;
+
+            friends *= 2;
+            friends = friends * 2;
+
+            friends /= 2;
+            friends = friends / 2;
+            
+            
+
+            Console.WriteLine(friends);
+           
+            Console.ReadKey();
+        }
+    }
+}
+
+// .\run_csharp
+// pour lancer le code
