692f88ff7d114020d34fde59c0d37280f1233035
Killian <martykillian7@gmail.com>
2025-06-03 09:22:35 +0200

    nested loops

--- nested-loops/Program.cs
+++ nested-loops/Program.cs
@@ -6,22 +6,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-          
-          Console.WriteLine("How many rows? ");
-          int rows = Convert.ToInt32(Console.ReadLine());
 
-          Console.WriteLine("How many columns? ");
-          int columns = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("How many rows? ");
+         int rows = Convert.ToInt32(Console.ReadLine());
 
-          Console.WriteLine("What symbol? ");
-          String symbol = Console.ReadLine();
+         Console.WriteLine("How many columns? ");
+         int columns = Convert.ToInt32(Console.ReadLine());
 
-          for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-              Console.Write(symbol);
-            }
-            Console.WriteLine();
+         Console.WriteLine("What symbol? ");
+         String symbol = Console.ReadLine();  
+
+         for (int i = 0; i < rows; i++) {
+          for (int j = 0; j < columns; j++) {
+            Console.Write(symbol);
           }
+          Console.WriteLine();
+         }
 
           Console.ReadKey();
         }
