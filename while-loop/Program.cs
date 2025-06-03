f795ef70c458fd5f0a99873069f207cc99b84b30
Killian <martykillian7@gmail.com>
2025-06-03 09:09:28 +0200

    while loop

---
+++ while-loop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter your name: ");
+           String name = Console.ReadLine();
+
+            // Répète la boucle tant que le nom n'est pas entré
+           while (name == "") {
+            Console.WriteLine("You did not enter your name");
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+           }
+
+           Console.WriteLine("Hello " + name);
+           
+            Console.ReadKey();
+        }
+    }
+}
+
+// dotnet new console pour créer un nouveau projet console
+// dotnet run pour exécuter le programme
+// dotnet build pour compiler le programme
