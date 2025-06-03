3765c4ccc7196b5ff38b0da206c32a5944853ef0
Killian <martykillian7@gmail.com>
2025-06-03 09:21:15 +0200

    nested-loops

---
+++ nested-loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          Console.WriteLine("How many rows? ");
+          int rows = Convert.ToInt32(Console.ReadLine());
+
+          Console.WriteLine("How many columns? ");
+          int columns = Convert.ToInt32(Console.ReadLine());
+
+          Console.WriteLine("What symbol? ");
+          String symbol = Console.ReadLine();
+
+          for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+              Console.Write(symbol);
+            }
+            Console.WriteLine();
+          }
+
+          Console.ReadKey();
+        }
+    }
+}
+
+// dotnet new console pour créer un nouveau projet console
+// dotnet run pour exécuter le programme
+// dotnet build pour compiler le programme
