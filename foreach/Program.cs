11157b32b1ad1bd36a0b19937595c67fd29e8a04
Killian <martykillian7@gmail.com>
2025-06-04 09:23:37 +0200

    foreach loop

---
+++ foreach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // Pour créer un Array, on peut le faire de la façon suivante :
+          String[] cars = {"BMW", "Ford", "Toyota", "Honda"};
+
+          // pour chaque élément de l'Array, on peut le faire de la façon suivante :
+          foreach(string car in cars) {
+            Console.WriteLine(car);
+          }
+
+          Console.ReadKey();
+        }
+    }
+}
+
+// dotnet new console pour créer un nouveau projet console
+// dotnet run pour exécuter le programme
+// dotnet build pour compiler le programme
