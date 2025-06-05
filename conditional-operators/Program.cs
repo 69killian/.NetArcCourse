a77e58ec5264a86b5e1f8d38da3e8625528c0fe8
Killian <martykillian7@gmail.com>
2025-06-05 09:14:19 +0200

    ternary operator

---
+++ conditional-operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double temperature = 20;
+           string message;
+           message = (temperature >= 15) ? "It is warm" : "It is cold";
+           Console.WriteLine(message);
+
+          Console.ReadKey();
+        }
+    }
+}
+
+// dotnet new console pour créer un nouveau projet console
+// dotnet run pour exécuter le programme
+// dotnet build pour compiler le programme
