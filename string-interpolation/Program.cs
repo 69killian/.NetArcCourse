b5ef0aba87d55294e51fcaa637d3bdf65d2b16a2
Killian <martykillian7@gmail.com>
2025-06-05 09:18:22 +0200

    string-interpolation

---
+++ string-interpolation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           String firstName = "John";
+           String lastName = "Doe";
+           int age = 25;
+
+            // Si je met un dollar devant les variables, je peux les mettre dans le string sans concaténer
+           Console.WriteLine($"Nom : {lastName}, Prénom : {firstName}, Age : {age}");
+
+           Console.ReadKey();
+        }
+    }
+}
+
+// dotnet new console pour créer un nouveau projet console
+// dotnet run pour exécuter le programme
+// dotnet build pour compiler le programme
