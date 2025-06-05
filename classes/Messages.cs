1da9de6aa3568f96d97bef2fbcdaad8e3313593d
Killian <martykillian7@gmail.com>
2025-06-05 09:38:39 +0200

    classes

---
+++ classes/Messages.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld
+{
+    static class Messages {
+        public static void Hello() {
+            Console.WriteLine("Hello welcome");
+        }
+        public static void Waiting() {
+            Console.WriteLine("Waiting For something");
+        }
+        public static void Bye() {
+            Console.WriteLine("Bye Bye good luck");
+        }
+    }
+}
---
+++ classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Messages.Hello();
+          Messages.Waiting();
+          Messages.Bye();
+           
+           Console.ReadKey();
+        }
+    }
+}
+
+// dotnet new console pour créer un nouveau projet console
+// dotnet run pour exécuter le programme
+// dotnet build pour compiler le programme
