ceef8947d9d0190b5966ecebf656b51e9d8c9b3e
Killian <martykillian7@gmail.com>
2025-06-05 10:01:22 +0200

    constructors

---
+++ constructor-car/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Car car1 = new Car("Chevy", "Corvette", 2022, "blue");
+           Car car2 = new Car("Ford", "Mustang", 2023, "red");
+
+           Car.Start();
+           Car.Stop();
+           Console.ReadKey();
+        }
+    }
+    class Car
+    {
+        String make;
+        String model;
+        int year;
+        String color;
+
+        public Car(String make, String model, int year, String color) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+        public static void Start() {
+            Console.WriteLine("The car starts");
+        }
+        public static void Stop() {
+            Console.WriteLine("The car stops");
+        }
+    }
+}
+
+// dotnet new console pour créer un nouveau projet console
+// dotnet run pour exécuter le programme
+// dotnet build pour compiler le programme
---
+++ constructor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instancier un objet
+            // comme le Random random = new Random();
+          Human human1 = new Human("Rick", 65);
+          Human human2 = new Human("Morty", 16);
+
+          human1.Eat();
+          human1.Sleep();
+
+          human2.Eat();
+          human2.Sleep();
+           
+           Console.ReadKey();
+        }
+    }
+    class Human 
+    {
+        // public rend accessible à d'autres classes ce qui est dedans
+        // les variables sont des champs que l'objet possède
+        public String name;
+        public int age;
+
+        // constructeur qui initialise les variables pour éviter d'avoir à les initialiser dans une autre classe
+        public Human(String name, int age) 
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // les méthodes sont des actions que l'objet peut faire
+        public void Eat()
+        {
+            Console.WriteLine(name + 'is eating');
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(name + 'is sleeping');
+        }
+    }
+}
+
+// dotnet new console pour créer un nouveau projet console
+// dotnet run pour exécuter le programme
+// dotnet build pour compiler le programme
