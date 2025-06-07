478baeff0e34d65060906ba0352caddef426bc8b
Killian <martykillian7@gmail.com>
2025-06-07 10:22:32 +0200

    getters-setter

---
+++ getters-setters/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(200);
+            car.Speed = 100;
+            Console.WriteLine(car.Speed);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        private int speed;
+
+        public Car(int speed)
+        {
+            this.speed = speed;
+        }
+
+        public int Speed
+        {
+            // get = récupère la valeur de la variable
+            get { return speed; }
+            // set = définit la valeur de la variable
+            set 
+            {
+                if (speed > 500) 
+                {
+                    speed = 500;
+                } 
+            else 
+            { 
+                speed = value; 
+            }
+            }
+        }
+    }
+}
+// dotnet new console pour créer un nouveau projet console
+// dotnet run pour exécuter le programme
+// dotnet build pour compiler le programme
